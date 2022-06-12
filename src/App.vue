0dc2127830d0696d0b7066d3432c65f5c5b66d3d
twinny-beongae <beongae.twinny@gmail.com>
2022-06-12 22:02:19 +0900

    구글로 로그인 후 페이지 전

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/event">Event</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/EventView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h4>Event</h4>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'EventView',
+  components: {
+  }
+}
+</script>
