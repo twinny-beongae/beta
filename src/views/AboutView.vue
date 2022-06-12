648035efb5406acd4419f35c92fefaa332021279
twinny-beongae <beongae.twinny@gmail.com>
2022-06-12 14:36:05 +0900

    파이어베이스 실시간db 연결 및 값 가져오기 테스트

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,35 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, onValue } from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAXzw2BNqhI6HJLFvDxGk8aFNnGEGz7ge0',
+  authDomain: 'twinny-beongae-ea0a4.firebaseapp.com',
+  databaseURL: 'https://twinny-beongae-ea0a4-default-rtdb.firebaseio.com',
+  projectId: 'twinny-beongae-ea0a4',
+  storageBucket: 'twinny-beongae-ea0a4.appspot.com',
+  messagingSenderId: '82638857824',
+  appId: '1:82638857824:web:ef5ea88e03accb25bc4505',
+  measurementId: 'G-1FVMVDJYTB'
+}
+
+const app = initializeApp(firebaseConfig)
+
+// Get a reference to the database service
+const db = getDatabase(app)
+const appointmentTime = ref(db, 'rooms/' + 'room1' + '/appointment_time')
+onValue(appointmentTime, (snapshot) => {
+  const data = snapshot.val()
+  console.log(data)
+})
+
+export default {
+  name: 'AboutView',
+  components: {
+  }
+}
+</script>
